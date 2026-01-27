4d31ec0a1a2e1fe94d387e603d6e7b01eca3b372
Kacper Margol <kacpermargol@gmail.com>
2026-01-27 10:44:54 +0100

    feat: Implement Google Gemini API integration for content generation, securing API key via environment variables and .gitignore. add: promt and response token count prints

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if api_key is None:
+    print("GEMINI_API_KEY not found in .env file")
+    exit(1)
+
+client = genai.Client(api_key=api_key)
+
+model_id = "gemini-2.5-flash"
+contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+response = client.models.generate_content(
+    model=model_id,
+    contents=contents,
+)
+
+if response.usage_metadata is None:
+    raise RuntimeError("API request failed: usage_metadata is missing")
+
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+print(f"Response:\n {response.text}")
+
 def main():
     print("Hello from k-os!")
 
