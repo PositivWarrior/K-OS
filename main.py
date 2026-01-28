873c7858035222156d4dc93a4840931225210ae1
Kacper Margol <kacpermargol@gmail.com>
2026-01-28 15:57:31 +0100

    feat: Implement command-line argument parsing for user prompts and encapsulate API interaction within the main function.

--- main.py
+++ main.py
@@ -1,36 +1,37 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-
-api_key = os.environ.get("GEMINI_API_KEY")
-
-if api_key is None:
-    print("GEMINI_API_KEY not found in .env file")
-    exit(1)
-
-client = genai.Client(api_key=api_key)
-
-model_id = "gemini-2.5-flash"
-contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-
-response = client.models.generate_content(
-    model=model_id,
-    contents=contents,
-)
-
-if response.usage_metadata is None:
-    raise RuntimeError("API request failed: usage_metadata is missing")
-
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
-print(f"Response:\n {response.text}")
-
 def main():
-    print("Hello from k-os!")
+    parser = argparse.ArgumentParser(description="K-OS: Agentic AI Enviorment")
+    parser.add_argument("user_prompt", type=str, help="The prompt to send to the AI Agent")
+    args = parser.parse_args()
 
+    load_dotenv()
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if api_key is None:
+        print("GEMINI_API_KEY not found in .env file")
+        exit(1)
+
+    client = genai.Client(api_key=api_key)
+
+    model_id = "gemini-2.5-flash"
+    contents = args.user_prompt
+
+    response = client.models.generate_content(
+        model=model_id,
+        contents=contents,
+        )
+
+    if response.usage_metadata is None:
+        raise RuntimeError("API request failed: usage_metadata is missing")
+
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response:\n {response.text}")
 
 if __name__ == "__main__":
     main()
