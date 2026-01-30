3bcae9ca5f3f8e8ef789d39f6cccd82fc6de38ad
Kacper Margol <kacpermargol@gmail.com>
2026-01-30 10:00:39 +0100

    refactor: Format user prompt as `types.Content` for `generate_content` API call.

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     parser = argparse.ArgumentParser(description="K-OS: Agentic AI Enviorment")
     parser.add_argument("user_prompt", type=str, help="The prompt to send to the AI Agent")
     args = parser.parse_args()
 
+    messages = [
+        types.Content(
+            role="user", 
+            parts=[types.Part(text=args.user_prompt)]
+        )
+    ]
+    
     load_dotenv()
 
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -19,11 +27,10 @@ def main():
     client = genai.Client(api_key=api_key)
 
     model_id = "gemini-2.5-flash"
-    contents = args.user_prompt
 
     response = client.models.generate_content(
         model=model_id,
-        contents=contents,
+        contents=messages
         )
 
     if response.usage_metadata is None:
