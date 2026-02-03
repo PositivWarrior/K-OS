fc712df4575aabce9ce59a9b44ebe75283e4c269
Kacper Margol <kacpermargol@gmail.com>
2026-02-03 10:12:41 +0100

    feat: Add system prompt from a new module and configure content generation with it, setting temperature to 0.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
 
 def main():
     parser = argparse.ArgumentParser(description="K-OS: Agentic AI Enviorment")
@@ -31,8 +32,12 @@ def main():
 
     response = client.models.generate_content(
         model=model_id,
-        contents=messages
+        contents=messages,
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompt,
+            temperature=0
         )
+    )
 
     if response.usage_metadata is None:
         raise RuntimeError("API request failed: usage_metadata is missing")
